ab1d1e15f3720079015fd9a331f81164168c7318
krishna <krishnatodkar1@gmail.com>
2023-02-21 14:09:45 +0530

    adding changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("Hello World Through cmd Maven");
+        System.out.println("jenkins");
 	}
 }
